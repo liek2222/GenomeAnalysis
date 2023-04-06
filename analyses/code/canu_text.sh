44a8d14e9d12896571253164be60e4f95cae3ff3
Linnéa Ekström <liek2222@rackham1.uppmax.uu.se>
2023-04-06 09:07:07 +0200

    Canu configuration file

--- analyses/code/canu_text.sh
+++ analyses/code/canu_text.sh
@@ -7,10 +7,12 @@
 #SBATCH -J canu_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user liek2222@student.uu.se
+
 # Load modules
+
 module load bioinfo-tools
-module load ....
-# Your commands
-<Command_1...>
-<Command_2...>
+module load canu
+
+# Commands
+canu -p canu_primary_fasta -d canu_pacbio_primary -assemble /home/liek2222/GAproject/analyses/code/canu_configuration
 
